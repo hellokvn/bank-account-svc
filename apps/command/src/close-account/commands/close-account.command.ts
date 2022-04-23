3674a27202b051e526bddba2efc4dbf99acfb3d5
Kevin <hellokevinvogel@gmail.com>
2022-04-23 07:00:22 +0200

    chore(): rm deprecated

--- apps/command/src/close-account/commands/close-account.command.ts
+++
@@ -1,9 +0,0 @@
-import { CloseAccountDto } from '../controllers/close-account.dto';
-
-export class CloseAccountCommand {
-  public id: string;
-
-  constructor(command: CloseAccountDto) {
-    this.id = command.id;
-  }
-}

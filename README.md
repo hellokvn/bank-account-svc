b0498bf35ebeeca5c385c4a799b7418c79f5869a
Kevin <hellokevinvogel@gmail.com>
2022-05-02 18:51:26 +0200

    chore(): add medium article

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Description
 
 This repository is part of my article on Medium:  
-NOT PUBLISHED YET
+[Microservices with CQRS and Event Sourcing in TypeScript with NestJS](https://medium.com/gitconnected/microservices-with-cqrs-in-typescript-and-nestjs-5a8af0a56c3a)
 
 ## Repositories
 
--- apps/query/src/consumer/account-opened/consumer/account-opened.consumer.ts
+++ apps/query/src/consumer/account-opened/consumer/account-opened.consumer.ts
@@ -1,26 +1,15 @@
-import { Controller, Inject, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { EventBus } from '@nestjs/cqrs';
-import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { plainToClass } from 'class-transformer';
 import { KafkaMessage } from 'kafkajs';
 
 import { AccountOpenedEvent } from '@shared/events';
 
 @Controller()
-export class AccountOpenedConsumer implements OnApplicationBootstrap, OnApplicationShutdown {
-  @Inject('KAFKA_SERVICE')
-  private readonly client: ClientKafka;
-
+export class AccountOpenedConsumer {
   @Inject(EventBus)
   private readonly eventBus: EventBus;
-
-  public onApplicationBootstrap() {
-    this.client.subscribeToResponseOf('AccountOpenedEvent');
-  }
-
-  public onApplicationShutdown() {
-    this.client.close();
-  }
 
   @MessagePattern('AccountOpenedEvent')
   private consume(@Payload() { value }: KafkaMessage): void {
